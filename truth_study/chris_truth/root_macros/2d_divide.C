ef88035df031a7c02306809d3dbc3be715e50001
Chris Lucas <chris.lucas@bristol.ac.uk>
2012-03-19 18:51:48 +0000

    updated with preselection section

--- truth_study/chris_truth/root_macros/2d_divide.C
+++ truth_study/chris_truth/root_macros/2d_divide.C
@@ -1,13 +1,21 @@
 {
-
-//NEED TO KEEP MAKING PT CUT CHANGES
-
-TString histFileName = "AK5Calo_test_pset.root";
-
+TString histFileName = "AK5Calo_mSUGRA_m0_220to3000_m12_100to1000_tanb_10andA0_0_7TeV_chris_truth_all.root";
 TFile *_file0 = TFile::Open(histFileName, "UPDATE");
+//----------------------------------------------------------------------------------//
 
-TH2D *full_scan_bf, *scan_bmulti_frac_0gev_bf[4], *bmulti_scan_total_0gev_bf;
-TH2D *bmulti_scan_0gev_bf[4];
+TH2d *full_scan_bf, *full_scan_ps, *full_scan_fs;
+
+_file0->GetObject("before_cuts/mSUGRA_Scan_Plane;1", full_scan_bf);
+_file0->GetObject("preselection_cuts/mSUGRA_Scan_Plane;1", full_scan_ps);
+_file0->GetObject("fullselection_cuts/mSUGRA_Scan_Plane;1", full_scan_fs);
+//----------------------------------------------------------------------------------//
+///////////////////////////////////////////////////
+//-----------------------//
+// Analysis for no cuts  //
+//-----------------------//
+///////////////////////////////////////////////////
+
+TH2D *scan_bmulti_frac_0gev_bf[4], *bmulti_scan_total_0gev_bf, *bmulti_scan_0gev_bf[4];
 
 TH2D *scan_bmulti_frac_0gev_bf[0] = new TH2D("bf_frac_0gev_0bs", "bf_frac_0gev_0bs", 150, 0, 3000, 51, 0, 1020);
 TH2D *scan_bmulti_frac_0gev_bf[1] = new TH2D("bf_frac_0gev_1bs", "bf_frac_0gev_1bs", 150, 0, 3000, 51, 0, 1020);
@@ -17,7 +25,7 @@ TH2D *scan_bmulti_frac_0gev_bf[3] = new TH2D("bf_frac_0gev_>2bs", "bf_frac_0gev_
 TH2D *bmulti_scan_total_0gev_bf = new TH2D("total_b_0gev_bf", "total_b_0gev_bf", 150, 0, 3000, 51, 0, 1020);
 TH2D *bmulti_scan_total_frac_0gev_bf = new TH2D("total_b_frac_0gev_bf", "total_b_frac_0gev_bf", 150, 0, 3000, 51, 0, 1020);
 
-_file0->GetObject("before_cuts/mSUGRA_Scan_Plane;1", full_scan_bf);
+
 _file0->GetObject("before_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_0;1", bmulti_scan_0gev_bf[0]);
 _file0->GetObject("before_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_1;1", bmulti_scan_0gev_bf[1]);
 _file0->GetObject("before_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_2;1", bmulti_scan_0gev_bf[2]);
@@ -26,7 +34,7 @@ _file0->GetObject("before_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_3;1", bmulti_sc
 bmulti_scan_total_0gev_bf->Add(bmulti_scan_0gev_bf[1],bmulti_scan_0gev_bf[2]);
 bmulti_scan_total_0gev_bf->Add(bmulti_scan_0gev_bf[3]);
 
-cout << "Filling fractions for before cuts" << endl;
+cout << "Filling fractions for before any cuts" << endl;
 
 for(Int_t i=0; i<=3; i++){
 	scan_bmulti_frac_0gev_bf[i]->Divide(bmulti_scan_0gev_bf[i], full_scan_bf);
@@ -34,9 +42,47 @@ for(Int_t i=0; i<=3; i++){
 
 bmulti_scan_total_frac_0gev_bf->Divide(bmulti_scan_total_0gev_bf, full_scan_bf);
 
+//----------------------------------------------------------------------------------//
 ///////////////////////////////////////////////////
 //-----------------------//
-// Repeat for af cuts    //
+// Analysis for ps cuts  //
+//-----------------------//
+///////////////////////////////////////////////////
+
+TH2D  *scan_bmulti_frac_0gev_ps[4], *bmulti_scan_total_0gev_ps, *bmulti_scan_0gev_ps[4];
+
+TH2D *scan_bmulti_frac_0gev_ps[0] = new TH2D("ps_frac_0gev_0bs", "ps_frac_0gev_0bs", 150, 0, 3000, 51, 0, 1020);
+TH2D *scan_bmulti_frac_0gev_ps[1] = new TH2D("ps_frac_0gev_1bs", "ps_frac_0gev_1bs", 150, 0, 3000, 51, 0, 1020);
+TH2D *scan_bmulti_frac_0gev_ps[2] = new TH2D("ps_frac_0gev_2bs", "ps_frac_0gev_2bs", 150, 0, 3000, 51, 0, 1020);
+TH2D *scan_bmulti_frac_0gev_ps[3] = new TH2D("ps_frac_0gev_>2bs", "ps_frac_0gev_>2bs", 150, 0, 3000, 51, 0, 1020);
+
+TH2D *bmulti_scan_total_0gev_ps = new TH2D("total_b_0gev_ps", "total_b_0gev_ps", 150, 0, 3000, 51, 0, 1020);
+
+TH2D *bmulti_scan_total_frac_0gev_ps = new TH2D("total_b_frac_0gev_ps", "total_b_frac_0gev_ps", 150, 0, 3000, 51, 0, 1020);
+
+
+_file0->GetObject("preselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_0;1", bmulti_scan_0gev_ps[0]);
+_file0->GetObject("preselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_1;1", bmulti_scan_0gev_ps[1]);
+_file0->GetObject("preselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_2;1", bmulti_scan_0gev_ps[2]);
+_file0->GetObject("preselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_3;1", bmulti_scan_0gev_ps[3]);
+
+
+cout << "Filling fractions for PreSelection cuts" << endl;
+
+for(Int_t i=0; i<=3; i++){
+	scan_bmulti_frac_0gev_ps[i]->Divide(bmulti_scan_0gev_ps[i], full_scan_ps);
+}
+
+// sum 1,2 and >2 plots for 'any' b-quark event
+bmulti_scan_total_0gev_ps->Add(bmulti_scan_0gev_ps[1],bmulti_scan_0gev_ps[2]);
+bmulti_scan_total_0gev_ps->Add(bmulti_scan_0gev_ps[3]);
+
+bmulti_scan_total_frac_0gev_ps->Divide(bmulti_scan_total_0gev_ps, full_scan_ps);
+
+//----------------------------------------------------------------------------------//
+///////////////////////////////////////////////////
+//-----------------------//
+// Anlaysis for fs cuts  //
 //-----------------------//
 ///////////////////////////////////////////////////
 
@@ -59,14 +105,14 @@ TH2D *scan_bmulti_0gev_eff[3] = new TH2D("eff_0gev_>2bs", "eff_0gev_>2bs", 150, 
 TH2D *scan_bmulti_total_0gev_eff = new TH2D("total_0gev_eff", "total_0gev_eff", 150, 0, 3000, 51, 0, 1020);
 TH2D *bmulti_scan_total_frac_0gev_fs = new TH2D("total_b_frac_0gev_fs", "total_b_frac_0gev_fs", 150, 0, 3000, 51, 0, 1020);
 
-_file0->GetObject("fullselection_cuts/mSUGRA_Scan_Plane;1", full_scan_fs);
+
 _file0->GetObject("fullselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_0;1", bmulti_scan_0gev_fs[0]);
 _file0->GetObject("fullselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_1;1", bmulti_scan_0gev_fs[1]);
 _file0->GetObject("fullselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_2;1", bmulti_scan_0gev_fs[2]);
 _file0->GetObject("fullselection_cuts/mSUGRA_b-quark_multi-0_gev_pt_cut_3;1", bmulti_scan_0gev_fs[3]);
 
 
-cout << "Filling fractions for after cuts" << endl;
+cout << "Filling fractions for FullSelection cuts" << endl;
 
 for(Int_t i=0; i<=3; i++){
 	scan_bmulti_frac_0gev_fs[i]->Divide(bmulti_scan_0gev_fs[i], full_scan_fs);
@@ -80,6 +126,8 @@ bmulti_scan_total_frac_0gev_fs->Divide(bmulti_scan_total_0gev_fs, full_scan_fs);
 
 scan_bmulti_total_0gev_eff->Divide(bmulti_scan_total_0gev_fs, bmulti_scan_total_0gev_bf);
 scan_bmulti_total_0gev_eff->GetZaxis()->SetRangeUser(0,0.2);
+
+//----------------------------------------------------------------------------------//
 
 cout << "Writing and closing " << histFileName << endl;
 
